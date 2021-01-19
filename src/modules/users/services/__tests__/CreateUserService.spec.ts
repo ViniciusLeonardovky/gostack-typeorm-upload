40664470249052292e3ca2bd52c2c51df1731f09
viniciusleonardovky <vile.pb@gmail.com>
2021-01-19 05:58:56 -0300

    test: create new user test

---
+++ src/modules/users/services/__tests__/CreateUserService.spec.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { Connection, getRepository, getConnection } from 'typeorm';
+import createConnection from '@shared/infra/typeorm/index';
+
+import { User } from '@modules/users/infra/typeorm/entities/User';
+
+import { app } from '@shared/infra/http/app';
+
+let connection: Connection;
+
+describe('User', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test-connection');
+
+    await connection.query('DROP TABLE IF EXISTS transactions');
+    await connection.query('DROP TABLE IF EXISTS categories');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  it('should be able to create a new user', async () => {
+    await request(app).post('/users').send({
+      name: 'Nome maneiro',
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email: 'teste@teste.com' },
+    });
+
+    expect(user?.name).toBe('Nome maneiro');
+    expect(user?.email).toBe('teste@teste.com');
+  });
+});
