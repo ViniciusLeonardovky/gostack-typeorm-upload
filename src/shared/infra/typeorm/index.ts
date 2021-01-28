3bce2ed196fa322032bc8d65c2f37c347d5265d1
viniciusleonardovky <vile.pb@gmail.com>
2021-01-28 02:17:55 -0300

    refactor: add enviroment variables

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -12,8 +12,18 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gofinances_tests'
-          : defaultOptions.database,
+          ? process.env.DB_DATABASE_TEST
+          : process.env.DB_DATABASE,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: ['./src/domains/**/infra/typeorm/entities/*.ts'],
+      migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+      cli: {
+        migrationsDir: './src/shared/infra/typeorm/migrations',
+      },
     }),
   );
 };
