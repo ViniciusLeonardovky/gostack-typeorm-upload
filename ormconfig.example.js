78c338c9bf6502936fa331955b84a36d6c869f27
viniciusleonardovky <vile.pb@gmail.com>
2021-02-01 03:46:18 -0300

    refactor: ormconfig settings

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -6,8 +6,8 @@ module.exports = {
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE,
-  "entities": ["./src/domains/**/infra/typeorm/entities/*.ts"],
-  "migrations": ["./src/shared/infra/typeorm/migrations/*.ts"],
+  "entities": process.env.NODE_ENV === 'dev' ? ["./src/domains/**/infra/typeorm/entities/*.ts"] : ["./dist/domains/**/infra/typeorm/entities/*.js"],
+  "migrations": process.env.NODE_ENV === 'dev' ? ["./src/shared/infra/typeorm/migrations/*.ts"] : ["./dist/shared/infra/typeorm/migrations/*.js"],
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
