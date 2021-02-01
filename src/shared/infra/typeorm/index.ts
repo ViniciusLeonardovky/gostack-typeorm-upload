634c20fac8ae854a2ee888b3b0d74185abc52f6d
viniciusleonardovky <vile.pb@gmail.com>
2021-02-01 02:54:03 -0300

    refactor: settings develop

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,22 +7,38 @@ import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 export default async (name = 'default'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  if (process.env.NODE_ENV === 'test') {
+    return createConnection(
+      Object.assign(defaultOptions, {
+        name,
+        database: process.env.DB_DATABASE_TEST,
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        entities: ['./src/domains/**/infra/typeorm/entities/*.ts'],
+        migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+        cli: {
+          migrationsDir: './src/shared/infra/typeorm/migrations',
+        },
+      }),
+    );
+  }
+
   return createConnection(
     Object.assign(defaultOptions, {
       name,
-      database:
-        process.env.NODE_ENV === 'test'
-          ? process.env.DB_DATABASE_TEST
-          : process.env.DB_DATABASE,
+      database: process.env.DB_DATABASE,
       type: 'postgres',
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      entities: ['./src/domains/**/infra/typeorm/entities/*.ts'],
-      migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+      entities: ['./dist/domains/**/infra/typeorm/entities/*.js'],
+      migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
       cli: {
-        migrationsDir: './src/shared/infra/typeorm/migrations',
+        migrationsDir: './dist/shared/infra/typeorm/migrations',
       },
     }),
   );
