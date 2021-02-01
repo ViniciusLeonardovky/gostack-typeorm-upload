ecbe8fbd5728ee1a99f777c16c3b84ab0adc7b2d
viniciusleonardovky <vile.pb@gmail.com>
2021-02-01 03:30:33 -0300

    refactor: settings develop

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,11 +5,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
+import '@shared/infra/typeorm';
 import '@shared/container';
 import { routes } from './routes';
-
-createConnection();
 
 export const app = express();
 
