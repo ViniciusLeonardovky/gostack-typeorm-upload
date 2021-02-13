2b7f9c1fa469b4be806eb96a081c6a20a94ba19d
viniciusleonardovky <vile.pb@gmail.com>
2021-02-13 18:23:10 -0300

    fix: ormconfig

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -5,7 +5,7 @@ module.exports = {
   "port": process.env.DB_PORT,
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_DATABASE,
+  "database": process.env.NODE_ENV === 'test' ? process.env.DB_DATABASE_TEST : process.env.DB_DATABASE,
   "entities": process.env.NODE_ENV === 'dev' ? ["./src/domains/**/infra/typeorm/entities/*.ts"] : ["./dist/domains/**/infra/typeorm/entities/*.js"],
   "migrations": process.env.NODE_ENV === 'dev' ? ["./src/shared/infra/typeorm/migrations/*.ts"] : ["./dist/shared/infra/typeorm/migrations/*.js"],
   "cli": {
