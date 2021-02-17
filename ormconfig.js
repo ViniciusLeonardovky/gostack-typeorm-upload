038a61199788af52c2ad78a1dd0f3d5dbd968a3a
ViniciusLeonadovky <vile.pb@gmail.com>
2021-02-17 18:15:09 -0300

    chore: adjust on ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,24 @@
 module.exports = {
-  "name": "default",
-  "type": "postgres",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.NODE_ENV === 'test' ? process.env.DB_DATABASE_TEST : process.env.DB_DATABASE,
-  "entities": process.env.NODE_ENV === 'dev' ? ["./src/domains/**/infra/typeorm/entities/*.ts"] : ["./dist/domains/**/infra/typeorm/entities/*.js"],
-  "migrations": process.env.NODE_ENV === 'dev' ? ["./src/shared/infra/typeorm/migrations/*.ts"] : ["./dist/shared/infra/typeorm/migrations/*.js"],
-  "cli": {
-    "migrationsDir": "./src/shared/infra/typeorm/migrations"
+  name: 'default',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === 'test'
+      ? process.env.DB_DATABASE_TEST
+      : process.env.DB_DATABASE,
+  entities:
+    process.env.NODE_ENV === 'dev'
+      ? ['./src/domains/**/infra/typeorm/entities/*.ts']
+      : ['./dist/domains/**/infra/typeorm/entities/*.js'],
+  migrations:
+    process.env.NODE_ENV === 'dev'
+      ? ['./src/shared/infra/typeorm/migrations/*.ts']
+      : ['./dist/shared/infra/typeorm/migrations/*.js'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations',
   },
-  "ssl": true,
-  "extra": {
-    "ssl": {
-      "rejectUnauthorized": false
-    }
-  }
-}
+  ssl: process.env.NODE_ENV === 'dev' ? false : true,
+};
